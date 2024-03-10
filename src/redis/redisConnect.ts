b1df021207d1681c64420a47749e0ae839dec5df
sudeesh kumar <sudeeshkumar@sudeeshs-MacBook-Air.local>
2024-03-10 12:11:10 +0530

    added changes in Radis

--- src/redis/redisConnect.ts
+++ src/redis/redisConnect.ts
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 config();
 
 const client = createClient({
-  password: String(process.env.REDIS_PASSWRD),
+  password: String(process.env.REDIS_PASSWORD),
   socket: {
     host: process.env.REDIS_HOST,
     port: 14951,
