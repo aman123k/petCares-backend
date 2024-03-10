75490fde2a2bbcb4a27f526621540cf8c0e97fd3
sudeesh kumar <sudeeshkumar@sudeeshs-MacBook-Air.local>
2024-03-10 18:05:52 +0530

    add change in db

--- src/db/connectDb.ts
+++ src/db/connectDb.ts
@@ -4,8 +4,8 @@ const connectDb = async (DATABASE_URL: string): Promise<void> => {
   try {
     await mongoose.connect(DATABASE_URL);
     console.log("connection successfull");
-  } catch {
-    console.log("connot connect db");
+  } catch (err) {
+    console.log("connot connect db", err);
   }
 };
 export default connectDb;
