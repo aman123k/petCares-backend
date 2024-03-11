f1a00b89b727eb49ad3708f6ab46b6d8531cb18d
sudeesh kumar <sudeeshkumar@sudeeshs-MacBook-Air.local>
2024-03-11 19:24:19 +0530

    excide limit to 50mb

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,8 @@ async function startServer() {
       res.json({ status: "ok" });
     });
     app.use(cookieParser());
-    app.use(express.json({ limit: "20mb" }));
-    app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+    app.use(express.json({ limit: "50mb" }));
+    app.use(express.urlencoded({ extended: true, limit: "50mb" }));
     app.use(
       cors({
         credentials: true,
